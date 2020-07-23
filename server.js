efdbcd1eb84bdfb73e350ed4576a29a73eaecd04
CareZaZa <carezaza@outlook.com>
2020-07-24 02:31:37 +0700

    '.'

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const { config } = require("dotenv");
-config();
+if(process.env.NODE_ENV === 'development'){
+  require("dotenv").config();
+}
+
 const express = require("express");
 const useMiddleWares = require("./config/use-middleWares");
 const connectMongoDb = require("./config/mongoose");
