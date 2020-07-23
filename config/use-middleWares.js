6ca113d48169d231beb0bbe65d1ad8469ec8f0c2
CareZaZa <carezaza@outlook.com>
2020-07-24 02:57:17 +0700

    ....

--- config/use-middleWares.js
+++ config/use-middleWares.js
@@ -1,14 +1,13 @@
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const compression = require("compression");
-const morgan = require("morgan");
 const fileUpload = require("express-fileupload");
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === "production") {
     app.use(compression());
   } else {
-    app.use(morgan("dev"));
+    app.use(require("morgan")("dev"));
   }
   app.use(cookieParser());
   app.use(fileUpload({ limits: { fileSize: 10 * 1024 * 1024 } }));
